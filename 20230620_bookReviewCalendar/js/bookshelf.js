5aef661f6dabbc198f2b90e8c0c6f629fe821d19
slhl0116 <osubin167@gmail.com>
2023-06-20 16:09:08 +0900

    bookshelf js, 이미지 10개 가져오쟈

---
+++ 20230620_bookReviewCalendar/js/bookshelf.js
@@ -0,0 +1,18 @@
+console.log(books[3].img)
+
+// HTML => js
+// .book-item > img 틀 가져오쟈
+const bookItems = document.getElementsByClassName("book-item"); //.book-item 덩어리 가져오기
+let i = 0;
+for(let bookItem of bookItems){ // .book-item 하나씩 꺼내자
+    const bookItemImg = bookItem.getElementsByTagName("img")[0]; //.book-item > img 0번째 꺼내자
+    bookItemImg.src = books[i].img; //거기에 data.js에서 데이터 가져와 설치
+    i++;
+}
+
+// const bookItems = documnet.getElementsByClassName("book-item");
+// console.log(bookItems);
+// bookItem = bookItems[0];
+// const bookItemImg = bookItem.getElementsByTagName("img")[0];
+// console.log(bookItemImg)
+// bookItemImg.src = books[3].img;
